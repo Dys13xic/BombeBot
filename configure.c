49b4310e109784847a76219f549817bddd94c0b4
Dys13xic <campbell.leckie@hotmail.com>
2023-04-13 00:16:18 -0400

    Default rotor turnover to Y position for historical consistency

--- configure.c
+++ configure.c
@@ -24,7 +24,7 @@ int configureRotors(int rotorModels[], ROTOR rotorSet[]) {
     for (i = 0; i < ROTOR_AMOUNT; i++) {
         strncpy(rotorSet[i].cipherAlphabet, cipherAlphabets[rotorModels[i]], ALPHA_LENGTH + 1);    // set ROTOR object's corresponding cipher alphabet
         rotorSet[i].position = 0;
-        rotorSet[i].turnover = 0;
+        rotorSet[i].turnover = 24;  // Bombe machine fast and middle rotor turnover on Y
     }
 
     return S_SUCCESS;
